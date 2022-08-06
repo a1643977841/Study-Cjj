53645f1e3e7070495ce146200f96e37172abb969
Liuhao <1643977841@qq.com>
2022-08-06 12:45:50 +0800

    122

--- undergraduate/chapter_9/section9_1/program9_1_3.cpp
+++ undergraduate/chapter_9/section9_1/program9_1_3.cpp
@@ -2,3 +2,36 @@
 // Created by ahao on 2022/7/17.
 // 对象排序程序
 //
+#include <iostream>
+using namespace std;
+template <typename T>
+int myCompare(const T &left, const T &right) {
+    if (left < right) {
+        return -1;
+    } else if (right < left) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+template <class T>
+void Swap(T &x, T &y) {
+    T tmp = x;
+    x = y;
+    y = tmp;
+}
+int main() {
+    string arrayString[10] = {"shang","xia", "zuo", "you", "qian", "hou", "dong", "xi", "nan", "bei"};
+    int j;
+    string temp;
+    for (int i = 0; i < 10; i++) {
+        j = i;
+        while (j > 0 && myCompare<string>(arrayString[j - 1], arrayString[j]) > 0) {
+            Swap(arrayString[j], arrayString[j-1]);
+            j --;
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << arrayString[i] << endl;
+    }
+}
--- undergraduate/chapter_9/section9_1/program9_1_4.cpp
+++ undergraduate/chapter_9/section9_1/program9_1_4.cpp
@@ -2,3 +2,45 @@
 // Created by ahao on 2022/7/17.
 // 重载函数模板
 //
+#include <iostream>
+using namespace std;
+class myDate{
+public:
+    myDate();
+    myDate(int, int, int);
+    friend ostream & operator << (ostream & os, const myDate &c);
+private:
+    int year, month, day;
+};
+myDate::myDate() {
+    year = 1970;
+    month = 1;
+    day = 1;
+}
+myDate::myDate(int y, int m, int d) {
+    year = y;
+    month = m;
+    day = d;
+}
+
+ostream &operator<<(ostream &os, const myDate &c) {
+    os << c.year << "/" << c.month << "/" << c.day;
+    return os;
+}
+template <class T1, class T2>
+void print(T1 arg1, T2 arg2) { 3
+    cout << arg1 << " , " << arg2 << endl;
+}
+template <class T>
+void print(T arg1, T arg2) {
+    cout << arg1 << " , " << arg2 << endl;
+}
+
+int main() {
+    int n = 1, m = 2;
+    print(n, m);
+    myDate d1, d2(2000, 1, 1);
+    print(d1, d2);
+    print(n, d1);
+    return 0;
+}
